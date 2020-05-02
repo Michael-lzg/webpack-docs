37c762053ceb4bda1ec9efed6e5f85160d5ad25a
lzg <398055960@qq.com>
2020-05-02 18:55:55 +0800

    update

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+
+set -e
+
+npm run docs:build
+
+cd docs/.vuepress/dist
+
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:Michael-lzg/webpack-docs.git master:gh-pages
+
+cd -
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -35,7 +35,7 @@ module.exports = {
       ]
     }
   },
-  dest: './docs/.vuepress/dist',
+  dest: './dist',
   ga: '',
   evergreen: true,
 }
